ca9b46a6b3aaefb914776dc5d08d7394dc3dcfd3
yama9973 <yamasasi@gmail.com>
2024-03-21 00:04:09 +0900

    Sitemap機能追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yama9973blog.netlify.app/',
+  integrations: [sitemap()]
+});
