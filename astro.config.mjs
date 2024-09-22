293e509a023564104e4ac1db088a46e1e22b864b
yama9973 <yamasasi@gmail.com>
2024-09-22 14:58:33 +0900

    netlify アダプタ導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yama9973blog.netlify.app/',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  output: "server",
+  adapter: netlify()
 });
